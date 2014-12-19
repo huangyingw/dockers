2b303781b688b5bf1749938c00705b4fb8a60283
huangyingw@linux_programming <huangyingw@gmail.com>
2014-12-18 23:48:46 -0800

    n

--- timemachine.sh
+++ timemachine.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -e PASSWORD=asdf --name=timemachine -v /media/volgrp/backup/:/backup -p 548:548 --net=host -d arve0/timemachine
+docker run -e PASSWORD=asdf -v /media/volgrp/timemachine/:/backup -p 548:548 --net=host -d arve0/timemachine
