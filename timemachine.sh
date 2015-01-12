8f838c671ae7cae30e576c4deb49534c080c3acf
huangyingw@linux_programming <huangyingw@gmail.com>
2015-01-11 23:30:08 -0800

    before initiating new branch dev from master,n

--- timemachine.sh
+++ timemachine.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -e PASSWORD=asdf -v /media/volgrp/timemachine/:/backup -p 548:548 --net=host -d arve0/timemachine
+sudo docker run -e PASSWORD=asdf -v /media/volgrp/timemachine/:/backup -p 548:548 --net=host -d arve0/timemachine
