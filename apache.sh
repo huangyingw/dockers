b8ba3ae77fb941a4285627e27f81b81fa5644b03
huangyingw@linux_programming <huangyingw@gmail.com>
2015-01-24 10:52:50 -0800

    n

--- apache.sh
+++ apache.sh
@@ -2,5 +2,7 @@
 docker run  \
   -v /root:/root \
   -v /media/volgrp:/media/volgrp \
+  -v /media/volgrp/images/etc/apache2:/etc/apache2 \
   -h apache \
-  -ti apache bash
+  -p 80:80 \
+  -ti huangyingw/apache bash
